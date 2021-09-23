fee7c713696cbbce4833bc347820a5839c4e48ee
Гарбовская Анастасия Алексеевна <garbovskaya_aa@etm.ru>
2021-09-24 01:53:48 +0300

    refactor: изменить классы по БЭМ

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,39 @@
+// Находим форму в DOM - редактирование профиля
 const popup = document.querySelector('.popup');
-let popupOpenBtn = document.querySelector('.menu_open_popup');
+
+// Находим кнопки - редактирование профиля
+let popupOpenBtn = document.querySelector('.profile__edit-button');
 let popupCloseBtn = popup.querySelector('.popup__close');
 
-let NameInput = popup.querySelector('.popup__input_username');
-let JobInput = popup.querySelector('.popup__input_job');
+// Находим поля формы в DOM - редактирование профиля 
+let NameInput = popup.querySelector('.popup__input_user_name');
+let JobInput = popup.querySelector('.popup__input_user_job');
 let NameProfile = document.querySelector('.profile__name');
 let JobProfile = document.querySelector('.profile__job');
 
-
-function popupToggle() {
-  popup.classList.toggle('popup_opened');
+// Функция открытие - редактирование профиля
+function popupOpen() {
+  popup.classList.add('popup_opened');
   NameInput.value = NameProfile.textContent;
   JobInput.value = JobProfile.textContent;
 }
 
-function formSubmitHandler(evt) {
-  evt.preventDefault(); 
-  NameProfile.textContent = NameInput.value;
-  JobProfile.textContent = JobInput.value;
+// Функция закрытие - редактирование профиля
+function popupClose() {
+  popup.classList.remove('popup_opened');
 }
 
-popupOpenBtn.addEventListener('click', popupToggle);
-popupCloseBtn.addEventListener('click', popupToggle);
+// Функция Обработчик «отправки» формы - редактирование профиля
+function formSubmitHandler(evt) {
+  evt.preventDefault(); 
+  let popupName = NameInput.value;
+  let popupJob = JobInput.value;
+  NameProfile.textContent = popupName;
+  JobProfile.textContent = popupJob;
+  popupClose()
+}
+
+// зарегистрировать обработчик события - редактирование профиля
+popupOpenBtn.addEventListener('click', popupOpen); // открыть
+popupCloseBtn.addEventListener('click', popupClose); // закрыть
 popup.addEventListener('submit', formSubmitHandler); //отправка формы
