7cd940281c39ec24c5cf081d069c37d1909b0e26
Гарбовская Анастасия Алексеевна <garbovskaya_aa@etm.ru>
2021-11-18 23:14:13 +0300

    feat: prevents default form behavior

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,12 +1,23 @@
 // включение валидации вызовом enableValidation
 // все настройки передаются при вызове
 
-enableValidation({
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_disabled',
-    inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__error_visible'
-  }); 
-  
+// enableValidation({
+//     formSelector: '.popup__form', // formElement
+//     inputSelector: '.popup__input', // formInput
+//     submitButtonSelector: '.popup__button',
+//     inactiveButtonClass: 'popup__button_disabled',
+//     inputErrorClass: 'popup__input_type_error', 
+//     errorClass: 'popup__error_visible'
+//   }); 
+enableValidation();
+
+function enableValidation() { // (должен быть объект с конфигом)
+  // const forms = [...document.querySelectorAll('.form__popup')];
+  const forms = Array.from(document.querySelectorAll('.popup__form'));
+  forms.forEach((form) => {
+    form.addEventListener('submit', (evt) => {
+      evt.preventDefault(); // предотвратить дефолтное поведение
+    })
+  })
+  console.log(forms);
+}
